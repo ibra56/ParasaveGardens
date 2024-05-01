d07ef8d040da4001ae507c1711195cf432b0cda5
hollytech30 <130698974+hollytech30@users.noreply.github.com>
2024-05-01 17:58:18 +0300

    Update dashboard.blade.php
    remove roles switcher

--- FarmLink/resources/views/pages/dashboard/dashboard.blade.php
+++ FarmLink/resources/views/pages/dashboard/dashboard.blade.php
@@ -1,8 +1,6 @@
 <x-app-layout>
     <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
         
-        @livewire('dashboard.self-choose-role-modal', key('dashboard-self-choose-role-modal'))
-
         <!-- Welcome banner -->
         <x-dashboard.welcome-banner />
 
