62d755d30aecc976b3f4787eb50d60670b796a9d
Ahairwe Jordan <2000809725@std.kyu.ac.ug>
2024-05-04 20:51:24 +0300

    rearrange the form section

--- resources/views/components/form-section.blade.php
+++ resources/views/components/form-section.blade.php
@@ -1,12 +1,12 @@
 @props(['submit'])
 
-<div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-3 md:gap-6']) }}>
+<div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-1 md:gap-6']) }}>
     <x-section-title>
         <x-slot name="title">{{ $title }}</x-slot>
         <x-slot name="description">{{ $description }}</x-slot>
     </x-section-title>
 
-    <div class="mt-5 md:mt-0 md:col-span-2">
+    <div class="mt-5 md:mt-0 md:col-span-1">
         <form wire:submit.prevent="{{ $submit }}">
             <div class="px-4 py-5 bg-white dark:bg-slate-800 sm:p-6 shadow {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
                 <div class="grid grid-cols-6 gap-6">
