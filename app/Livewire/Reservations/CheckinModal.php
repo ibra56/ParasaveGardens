bec84ec132ba7cb54c5572051f66a7e60b5717f9
Nnyenje Ibrahim <82960501+ibra56@users.noreply.github.com>
2024-05-31 01:08:51 +0300

    check in model

---
+++ app/Livewire/Reservations/CheckinModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Reservations;
+
+use Livewire\Component;
+
+class CheckinModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.reservations.checkin-modal');
+    }
+}
