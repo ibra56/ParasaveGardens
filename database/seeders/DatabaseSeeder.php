30ab13b6e9ba9ca1aa5469dd8977ae476eebe3c0
Ahairwe Jordan <2000809725@std.kyu.ac.ug>
2024-06-21 13:49:37 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     {
 
         $this->call([
-            UserMgtPermissions::class,
+            userMgtPermissions::class,
             DashboardTableSeeder::class,
 
             ExpenseCategorySeeders::class,
